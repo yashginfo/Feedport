27f39e32a20954c5f6961753b504df06c63b7ccb
AdityaS26 <adityasharma84453@gmail.com>
2025-04-04 21:13:56 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -2,5 +2,16 @@
 $inputname = $_POST['inputname'];
 $inputEmail = $_POST['inputEmail'];
 $inputPassword = $_POST['inputPassword'];
-$input
+$inputconfirmPassword = $POST['inputconfirmPassword'];
+$con = new mysqli('localhost','root','','signup');
+if($con->connect_error){
+  die('Connection failed :'.$con->connect_error);
+}
+else{
+  $stmt = $conn->prepare("insert into registration("inputname,inputEmail,inputPassword,inputconfirmpassword) values(?,?,?,?)");
+  $stmt->bind_param("ssss",$inputname,$inputEmail,$inputPassword,$inputconfirmPassword);
+  $stmt->execute();
+  echo "Registration sucessfull...";
+  $stmt->close();
+  $conn->close();
 ?>
